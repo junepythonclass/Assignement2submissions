cd0374bd59e6cf6b57df3e0e063b19ab0b5d344e
edinahhh <whitneykirabo@gmail.com>
2026-06-27 10:42:38 +0300

    Add files via upload

---
+++ work.py
@@ -0,0 +1,19 @@
+name = "whitney kirabo"
+name = input("what is your name?:")
+age = input("how old are you?")
+print(age)
+location = input("what is your location?")
+phonenumber = input("what is your phonenumber?")
+print(phonenumber)
+track = input("what track are you taking?")
+cohort = input("which cohort are you in?")
+
+ages = [15,22,34,20]
+if ages != 15:
+    print("not eligible for the course.")
+
+tracks = ["python", "javascript"]
+if track not in tracks:
+    print("not eligible for cse")
+
+# KIRABO.WHITNEY.EDINAH ASSIGNMENT
