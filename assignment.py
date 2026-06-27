8a93ba84ff5f19c5dae7aaa7b05bcc28e9b5fac1
pimermercy16-cyber <pimermercy16@gmail.com>
2026-06-27 11:01:04 +0300

    Add files via upload

--- assignment.py
+++ assignment.py
@@ -3,13 +3,14 @@
 #if someone is below 15, computer should say not eligible for the course, if track is not py or javas, computer should say not eligible for CSE
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
-location = input("Enter your localtion: ")
-phonenumber = ("Enter your phone number: ")
+location = input("Enter your location: ")
+phonenumber = input("Enter your phone number: ")
 track = input("Enter your track (python or javascript): ")
 cohort = input("Enter cohort intake: ")
+
 if age < 15:
     print("Not eligible for course")
-elif track not in ["python","javascript"]:
+elif track.lower() not in ["python", "javascript"]:
     print("Not eligible for CSE")
 else:
     print(f"\nSuccess! Welcome {name} to the {track.title()} track for {cohort}.")
