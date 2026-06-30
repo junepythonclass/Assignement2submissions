b48a668a9c2c19f606ebc14c2bcb23139a7ae877
pimermercy16-cyber <pimermercy16@gmail.com>
2026-06-30 13:49:23 +0300

    Add files via upload

--- assignment2.py
+++ assignment2.py
@@ -1,26 +1,16 @@
-# Ask user for details
-
+#Assignment
+#write a python program that asks a user to input their name, age, location, phone number,their track(python or javascript) and the cohort intake
+#if someone is below 15, computer should say not eligible for the course, if track is not py or javas, computer should say not eligible for CSE
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 location = input("Enter your location: ")
-phone = input("Enter your phone number: ")
-track = input("Enter your track (Python or JavaScript): ")
-cohort = input("Enter your cohort intake: ")
-
-# Check eligibility
+phonenumber = input("Enter your phone number: ")
+track = input("Enter your track (python or javascript): ")
+cohort = input("Enter cohort intake: ")
 
 if age < 15:
-    print("Not eligible for the course")
-
+    print("Not eligible for course")
 elif track.lower() not in ["python", "javascript"]:
-    print("Wrong course please try again ")
-
+    print("Not eligible for CSE")
 else:
-    print("\n===== STUDENT DETAILS =====")
-    print("Name:", name)
-    print("Age:", age)
-    print("Location:", location)
-    print("Phone Number:", phone)
-    print("Track:", track)
-    print("Cohort Intake:", cohort)
-    print("Eligible for the course")
+    print(f"\nSuccess! Welcome {name} to the {track.title()} track for {cohort}.")
